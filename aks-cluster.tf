79df7121c94347693b7cd32afac9ed9571277ff5
Nick Thomas <nicktho@microsoft.com>
2022-09-15 10:26:52 -0400

    first commit

---
+++ aks-cluster.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.naming_prefix}-rg"
+  location = "Central US"
+
+  tags = {
+    environment = var.naming_prefix
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${var.naming_prefix}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${var.naming_prefix}-aks-dns"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    //os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = var.naming_prefix
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+appId    = "d147a7b9-585e-4f82-852a-59adba3779db"
+password = "jXO6C_IpeVLzmUrCDktCiClHER1oTlj5D5"
+naming_prefix = "kubeclub"
